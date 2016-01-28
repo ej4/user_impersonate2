14c21955bb22b4553c3fcbc62f6fb8583f9e2ad9
Joshua Dayan <joshmdayan@gmail.com>
2016-01-28 10:39:50 -0600

    Prevent user from impersonating themselves

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 UserImpersonate::Engine.routes.draw do
-  get "/user/:user_id" , to: "impersonate#create", as: :impersonate_user
+  post "/user/:user_id" , to: "impersonate#create", as: :impersonate_user
   delete "/revert" , to: "impersonate#destroy", as: :revert_impersonate_user
 
   root to: "impersonate#index"
